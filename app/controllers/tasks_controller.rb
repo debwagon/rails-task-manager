c7b856416acbd0a6bcb4be1a200605f1c0b5fd84
deb wagon <deb.fsmk@gmail.com>
2020-05-13 01:43:22 +0800

    completed list all and show one, task_path id needs to ref task.id

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,11 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    # @id = params[:id]
+    # redirect_to task_path(@task)
+  end
+end
