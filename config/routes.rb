a87409269df9890cc012e5feca9dfa98b5b6b6ca
deb wagon <deb.fsmk@gmail.com>
2020-05-14 21:39:29 +0800

    delete need to put method

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get "tasks/:id", to: "tasks#show", as: :task
   get "new", to: "tasks#new"
   post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#delete", as: :delete
 end
