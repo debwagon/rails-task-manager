f1bca7abc1e9a4875ec89a41c7d0658f37913f2f
deb wagon <deb.fsmk@gmail.com>
2020-05-14 21:21:07 +0800

    form for binds the object model, and thus need to use plural for post route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,20 @@ class TasksController < ApplicationController
     # @id = params[:id]
     # redirect_to task_path(@task)
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params.require(:task).permit(:title, :details))
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
